22c2cdd0085f5bbba0877f4c99aa43d99d2d8e04
Namrata Bafna <namrata.bafna@siemens.com>
2020-10-11 11:22:53 +0530

    Sort based on dropdown selection

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,74 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <br>
+    
+     <b-dropdown aria-role="list" v-model="param"  @change="onChange($event)">
+            <button class="button active" slot="trigger">
+                <span>Sort By</span>                
+            </button>
+
+            <b-dropdown-item value="id" aria-role="listitem">Id</b-dropdown-item>
+            <b-dropdown-item value="text" aria-role="listitem">Text</b-dropdown-item>            
+    </b-dropdown>   
+    <br>
+    <br> 
+    <ul>
+      <li v-for="{id, text} in sortedActivityList" :key="id">{{ id }}   {{ text }}</li>
+    </ul>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  methods: {
+      onChange(event) {        
+        // sessionStorage.setItem('filter', event);
+        this.param = event;
+      }
+    },
+    data() {
+      return {
+        activityList: [
+        { id: 0, text: 'Run'},
+        { id: 1, text: 'Jump' },
+        { id: 2, text: 'Skip' },
+        { id: 3, text: 'Dance'},
+        { id: 4, text: 'Swing' },
+        { id: 5, text: 'Hop' },
+        { id: 6, text: 'Bounce' },
+        { id: 7, text: 'Crawl' },
+        { id: 8, text: 'Walk'},
+        { id: 9, text: 'Spin' },
+        { id: 10, text: 'Skate' },
+        { id: 11, text: 'Hike' }
+      ],
+
+        param: 'id'
+      };
+    },
+    computed: {      
+      sortedActivityList: function() {                 
+         
+          function sortById(a, b) {            
+            if (a.id < b.id) return -1;
+            if (a.id > b.id) return 1;
+            return 0;       
+          }
+
+          function sortByText(a, b) {
+            if (a.text < b.text) return -1;
+            if (a.text > b.text) return 1;  
+            return 0;     
+          }      
+        
+        return this.param == 'id' ? [...this.activityList].sort(sortById) : [...this.activityList].sort(sortByText);
+     }
+    }
+
 }
 </script>
 
